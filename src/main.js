f82523e12d5a1742d08c263aeeff28122d98fbd6
Safiqq <31335461+Safiqq@users.noreply.github.com>
2017-10-02 15:11:58 +0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,295 @@
+const LineAPI = require('./api');
+const { Message, OpType, Location } = require('../curve-thrift/line_types');
+let exec = require('child_process').exec;
+
+const myBot = ['u07bea77e0ddbe298a45f2758d834ce48','u79c68416a26d7db88b9d44042dafd4f5','u0e18f39c40973be5f201cc1b00528be4'];
+
+
+function isAdminOrBot(param) {
+    return myBot.includes(param);
+}
+
+
+class LINE extends LineAPI {
+    constructor() {
+        super();
+        this.receiverID = '';
+        this.checkReader = [];
+        this.stateStatus = {
+            cancel: 0,
+            kick: 0,
+        }
+    }
+
+    getOprationType(operations) {
+        for (let key in OpType) {
+            if(operations.type == OpType[key]) {
+                if(key !== 'NOTIFIED_UPDATE_PROFILE') {
+                    console.info(`[* ${operations.type} ] ${key} `);
+                }
+            }
+        }
+    }
+
+    poll(operation) {
+        if(operation.type == 25 || operation.type == 26) {
+            // console.log(operation);
+            const txt = (operation.message.text !== '' && operation.message.text != null ) ? operation.message.text : '' ;
+            let message = new Message(operation.message);
+            this.receiverID = message.to = (operation.message.to === myBot[0]) ? operation.message.from_ : operation.message.to ;
+            Object.assign(message,{ ct: operation.createdTime.toString() });
+            this.textMessage(txt,message)
+        }
+
+        if(operation.type == 13 && this.stateStatus.cancel == 1) {
+            this.cancelAll(operation.param1);
+        }
+
+        if(operation.type == 19) { //ada kick
+            // op1 = group nya
+            // op2 = yang 'nge' kick
+            // op3 = yang 'di' kick
+            if(!isAdminOrBot(operation.param3)) {
+                this._invite(operation.param1,[operation.param3]);
+            }
+            if(!isAdminOrBot(operation.param2)){
+                this._kickMember(operation.param1,[operation.param2]);
+            } 
+
+        }
+
+        if(operation.type == 55){ //ada reader
+
+            const idx = this.checkReader.findIndex((v) => {
+                if(v.group == operation.param1) {
+                    return v
+                }
+            })
+            if(this.checkReader.length < 1 || idx == -1) {
+                this.checkReader.push({ group: operation.param1, users: [operation.param2], timeSeen: [operation.param3] });
+            } else {
+                for (var i = 0; i < this.checkReader.length; i++) {
+                    if(this.checkReader[i].group == operation.param1) {
+                        if(!this.checkReader[i].users.includes(operation.param2)) {
+                            this.checkReader[i].users.push(operation.param2);
+                            this.checkReader[i].timeSeen.push(operation.param3);
+                        }
+                    }
+                }
+            }
+        }
+
+        if(operation.type == 13) { // diinvite
+            if(isAdminOrBot(operation.param2)) {
+                return this._acceptGroupInvitation(operation.param1);
+            } else {
+                return this._cancel(operation.param1,myBot);
+            }
+        }
+        this.getOprationType(operation);
+    }
+
+    async cancelAll(gid) {
+        let { listPendingInvite } = await this.searchGroup(gid);
+        if(listPendingInvite.length > 0){
+            this._cancel(gid,listPendingInvite);
+        }
+    }
+
+    async searchGroup(gid) {
+        let listPendingInvite = [];
+        let thisgroup = await this._getGroups([gid]);
+        if(thisgroup[0].invitee !== null) {
+            listPendingInvite = thisgroup[0].invitee.map((key) => {
+                return key.mid;
+            });
+        }
+        let listMember = thisgroup[0].members.map((key) => {
+            return { mid: key.mid, dn: key.displayName };
+        });
+
+        return { 
+            listMember,
+            listPendingInvite
+        }
+    }
+
+    setState(seq) {
+        if(isAdminOrBot(seq.from)){
+            let [ actions , status ] = seq.text.split(' ');
+            const action = actions.toLowerCase();
+            const state = status.toLowerCase() == 'on' ? 1 : 0;
+            this.stateStatus[action] = state;
+            this._sendMessage(seq,`Status: \n${JSON.stringify(this.stateStatus)}`);
+        } else {
+            this._sendMessage(seq,`Kamu bukan admin.`);
+        }
+    }
+
+    mention(listMember) {
+        let mentionStrings = [''];
+        let mid = [''];
+        for (var i = 0; i < listMember.length; i++) {
+            mentionStrings.push('@'+listMember[i].displayName+'\n');
+            mid.push(listMember[i].mid);
+        }
+        let strings = mentionStrings.join('');
+        let member = strings.split('@').slice(1);
+        
+        let tmp = 0;
+        let memberStart = [];
+        let mentionMember = member.map((v,k) => {
+            let z = tmp += v.length + 1;
+            let end = z - 1;
+            memberStart.push(end);
+            let mentionz = `{"S":"${(isNaN(memberStart[k - 1] + 1) ? 0 : memberStart[k - 1] + 1 ) }","E":"${end}","M":"${mid[k + 1]}"}`;
+            return mentionz;
+        })
+        return {
+            names: mentionStrings.slice(1),
+            cmddata: { MENTION: `{"MENTIONEES":[${mentionMember}]}` }
+        }
+    }
+
+    async leftGroupByName(payload) {
+        let gid = await this._findGroupByName(payload);
+        for (var i = 0; i < gid.length; i++) {
+            this._leaveGroup(gid[i]);
+        }
+    }
+    
+    async check(cs,group) {
+        let users;
+        for (var i = 0; i < cs.length; i++) {
+            if(cs[i].group == group) {
+                users = cs[i].users;
+            }
+        }
+        
+        let contactMember = await this._getContacts(users);
+        return contactMember.map((z) => {
+                return { displayName: z.displayName, mid: z.mid };
+            });
+    }
+
+    removeReaderByGroup(groupID) {
+        const groupIndex = this.checkReader.findIndex(v => {
+            if(v.group == groupID) {
+                return v
+            }
+        })
+
+        if(groupIndex != -1) {
+            this.checkReader.splice(groupIndex,1);
+        }
+    }
+
+    async textMessage(textMessages, seq) {
+        let [ cmd, ...payload ] = textMessages.split(' ');
+        payload = payload.join(' ');
+        let txt = textMessages.toLowerCase();
+        let messageID = seq.id;
+
+        if(cmd == 'cancel') {
+            if(payload == 'group') {
+                let groupid = await this._getGroupsInvited();
+                for (let i = 0; i < groupid.length; i++) {
+                    this._rejectGroupInvitation(groupid[i])                    
+                }
+                return;
+            }
+            if(this.stateStatus.cancel == 1) {
+                this.cancelAll(seq.to);
+            }
+        }
+
+        if(txt == 'response' || txt == 'respon') {
+            this._sendMessage(seq, 'Mira');
+        }
+
+	if(txt == 'keyword' || txt == 'help') {
+	    this._sendMessage(seq, '[Umum]:\n-cancel\n-respon/response\n-speed\n-point\n-reset\n-check\n-myid\n-open\n-close\n-join\n\n[Admin]:\n-kick on/off\n-kickall\n-cancel on/off\n-spm\n-left');
+	}
+
+        if(txt == 'speed') {
+            const curTime = (Date.now() / 1000);
+            await this._sendMessage(seq,'Processing....');
+            const rtime = (Date.now() / 1000) - curTime;
+            await this._sendMessage(seq, `${rtime} second(s)`);
+        }
+
+        if(txt === 'kickall' && this.stateStatus.kick == 1 && isAdminOrBot(seq.from)) {
+            let { listMember } = await this.searchGroup(seq.to);
+            for (var i = 0; i < listMember.length; i++) {
+                if(!isAdminOrBot(listMember[i].mid)){
+                    this._kickMember(seq.to,[listMember[i].mid])
+                }
+            }
+        }
+
+        if(txt == 'point') {
+            this._sendMessage(seq, `Read point telah di set!`);
+            this.removeReaderByGroup(seq.to);
+        }
+
+        if(txt == 'reset') {
+            this.checkReader = []
+            this._sendMessage(seq, `Read point telah di reset!`);
+        }  
+
+        if(txt == 'check'){
+            let rec = await this.check(this.checkReader,seq.to);
+            const mentions = await this.mention(rec);
+            seq.contentMetadata = mentions.cmddata;
+            await this._sendMessage(seq,mentions.names.join(''));
+            
+        }
+        if(seq.contentType == 13) {
+            seq.contentType = 0
+            this._sendMessage(seq,seq.contentMetadata.mid);
+        }
+	
+        const action = ['cancel on','cancel off','kick on','kick off']
+        if(action.includes(txt)) {
+            this.setState(seq)
+        }
+	
+        if(txt == 'myid') {
+            this._sendMessage(seq,`MID kamu: ${seq.from}`);
+        }
+
+        const joinByUrl = ['open','close'];
+        if(joinByUrl.includes(txt)) {
+            this._sendMessage(seq,`Updating group ...`);
+            let updateGroup = await this._getGroup(seq.to);
+            updateGroup.preventJoinByTicket = true;
+            if(txt == 'open') {
+                updateGroup.preventJoinByTicket = false;
+                const groupUrl = await this._reissueGroupTicket(seq.to)
+                this._sendMessage(seq,`line.me/R/ti/g/${groupUrl}`);
+            }
+            await this._updateGroup(updateGroup);
+        }
+
+        if(cmd == 'join') { //untuk join group pake qrcode contoh: join line://anu/g/anu
+            const [ ticketId ] = payload.split('g/').splice(-1);
+            let { id } = await this._findGroupByTicket(ticketId);
+            await this._acceptGroupInvitationByTicket(id,ticketId);
+        }
+
+        if(cmd == 'spm' && isAdminOrBot(seq.from)) { // untuk spam invite contoh: spm <mid>
+            for (var i = 0; i < 100; i++) {
+                this._createGroup(`SPAM BY SAFIQQ`,payload);
+                this._inviteMid(seq.to)
+            }
+        }
+        
+        if(cmd == 'left'  && isAdminOrBot(seq.from)) { //untuk left dari group atau spam group contoh left <alfath>
+            this.leftGroupByName(payload)
+        }
+
+    }
+
+}
+
+module.exports = new LINE();
