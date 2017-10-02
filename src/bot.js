ead74ec6d971f0cc2307f00e5708e8fec1cdd52c
Safiqq <31335461+Safiqq@users.noreply.github.com>
2017-10-02 16:41:21 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -3,7 +3,7 @@ let LINE = require('./main.js');
 
 const auth = {
 	authToken: 'ElsGkF4Al30VdZiQwPD4.dHJZNecxo/xYaCWBafkl5a.d4aZWVn5Z0n2N4KoiRz7xgPF4ybPvesnSXXukQWOdQY=',
-	certificate: '',
+	certificate: '4b4e19803a89beaa3f5dc3349a4f732075302f8d3ac684ec552083e439afd0b1',
 }
 // let client =  new LineConnect(auth);
 let client =  new LineConnect();
