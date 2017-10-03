b62a2c444a77690dd10d292e01e56eb00805a70c
Safiqq <31335461+Safiqq@users.noreply.github.com>
2017-10-03 20:09:14 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ const { Message, OpType, Location } = require('../curve-thrift/line_types');
 let exec = require('child_process').exec;
 
 const myBot = ['mid_kamu'];
+// -tips biar botnya gk error mulu-
+// ubah authtoken + certificate di src/bot.js
 
 
 function isAdminOrBot(param) {
